3e42236d3525eb14bf860443c105090f5379fbb0
hmkmartini <hmk.martini@gmail.com>
2023-05-28 19:57:53 -0300

    fix: d.ts types now exported as a module in order to work with squid-observability-nodejs bundle

--- squid_error.d.ts
+++ squid_error.d.ts
@@ -1,18 +1,19 @@
-export class SquidError extends Error {
+declare module 'squid-error' {
+  export class SquidError extends Error {
     static GetFullErrorStack(exception: any): any;
     static SerializeNativeError(error: any): {
-        syscall: any;
-        port: any;
-        path: any;
-        info: any;
-        errno: any;
-        dest: any;
-        address: any;
-        signal: any;
-        stack: any;
-        message: any;
-        name: any;
-        code: any;
+      syscall: any;
+      port: any;
+      path: any;
+      info: any;
+      errno: any;
+      dest: any;
+      address: any;
+      signal: any;
+      stack: any;
+      message: any;
+      name: any;
+      code: any;
     };
     static Serialize(error: any): any;
     static Create(settings: any, nativeError: any): SquidError;
@@ -35,43 +36,47 @@ export class SquidError extends Error {
     SetDetail(detail: any): SquidError;
     SetSkipLog(skipLog: any): SquidError;
     Serialize(): {
-        id: any;
-        detail: any;
-        timeStamp: any;
-        syscall: any;
-        port: any;
-        path: any;
-        info: any;
-        errno: any;
-        dest: any;
-        address: any;
-        signal: any;
-        stack: any;
-        message: any;
-        name: any;
-        code: any;
+      id: any;
+      detail: any;
+      timeStamp: any;
+      syscall: any;
+      port: any;
+      path: any;
+      info: any;
+      errno: any;
+      dest: any;
+      address: any;
+      signal: any;
+      stack: any;
+      message: any;
+      name: any;
+      code: any;
     };
-}
-export class SquidHttpError extends SquidError {
-    static Create(settings: any, nativeError: any): SquidHttpError;
-    httpStatusCode: any;
+    static IsSquidError(error: any): boolean;
+    static ExactInstanceOf(error: any): boolean;
+    static Convert(error: any, onlyConvertNonSquidErrors?: boolean): any;
+  }
+
+  export class SquidHttpError extends SquidError {
+    constructor(settings: any, nativeError: any, implementationContext: any)
     Serialize(): {
-        httpStatusCode: any;
-        id: any;
-        detail: any;
-        timeStamp: any;
-        syscall: any;
-        port: any;
-        path: any;
-        info: any;
-        errno: any;
-        dest: any;
-        address: any;
-        signal: any;
-        stack: any;
-        message: any;
-        name: any;
-        code: any;
+      httpStatusCode: any;
+      id: any;
+      detail: any;
+      timeStamp: any;
+      syscall: any;
+      port: any;
+      path: any;
+      info: any;
+      errno: any;
+      dest: any;
+      address: any;
+      signal: any;
+      stack: any;
+      message: any;
+      name: any;
+      code: any;
     };
     SetHttpStatusCode(status: any): SquidHttpError;
+  }
 }
