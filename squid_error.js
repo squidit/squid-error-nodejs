47a43938dfba291185c562259b1ff9ece356f55e
Marcelo Morais <marcelo.fernandes@squidit.com.br>
2023-10-31 13:57:05 -0300

    adição de jsdocs e geração automática de tipos

--- squid_error.js
+++ squid_error.js
@@ -1,7 +1,22 @@
+// @ts-check
 // Reference: https://www.bennadel.com/blog/2828-creating-custom-error-objects-in-node-js-with-error-capturestacktrace.htm
+
+/**
+ * @typedef {{ message?: string; stack?: string; code?: string; detail?: Record<string, unknown>; id?: number; timeStamp?: Date; skipLog?: boolean; }} SquidErrorSettings
+ */
+
+/**
+ * SquidHttpErrorSettings
+ * @typedef {(SquidErrorSettings & {httpStatusCode?: number})} SquidHttpErrorSettings
+ */
 
 class SquidError extends Error
 {
+  /**
+   * @param {SquidErrorSettings} settings
+   * @param {(Error | undefined)} nativeError
+   * @param {(...args: unknown[]) => unknown} implementationContext
+   */
   constructor (settings, nativeError, implementationContext)
   {
     super(settings?.message || nativeError?.message || 'An error occurred and no error message was set.');
@@ -10,7 +25,9 @@ class SquidError extends Error
 
     const providedStack = settings?.stack || nativeError?.stack;
 
+
     if (providedStack)
+      /** @type {string} stack */
       this.stack = providedStack;
     else
       // Capture the current stacktrace and store it in the property "this.stack". By
@@ -22,31 +39,46 @@ class SquidError extends Error
       Error.captureStackTrace(this, (implementationContext || this.constructor));
 
     // The parent constructor also sets the name property to "Error", so we reset it to the right value.
+    /** @type {string} */
     this.name      = this.constructor.name;
-    this.code      = settings?.code || nativeError?.code || 'ERROR_CODE_NOT_SET';
-    this.detail    = settings?.detail || '';
+    /** @type {string} */
+    this.code      = settings?.code || ('code' in nativeError && typeof nativeError.code === 'string' && nativeError?.code) || 'ERROR_CODE_NOT_SET';
+    /** @type {Record<string, unknown>} */
+    this.detail    = settings?.detail || {};
+    /** @type {number} */
     this.id        = settings?.id || 0;
+    /** @type {Date} */
     this.timeStamp = settings?.timeStamp || new Date();
+    /** @type {boolean} */
     this.skipLog   = settings?.skipLog || false;
 
+    /** @type {boolean} @private */
     this._isSquidError = true;
-
-    if (nativeError?.signal)  this.signal  = nativeError.signal;
-    if (nativeError?.address) this.address = nativeError.address;
-    if (nativeError?.dest)    this.dest    = nativeError.dest;
-    if (nativeError?.errno)   this.errno   = nativeError.errno;
-    if (nativeError?.info)    this.info    = nativeError.info;
-    if (nativeError?.path)    this.path    = nativeError.path;
-    if (nativeError?.port)    this.port    = nativeError.port;
-    if (nativeError?.syscall) this.syscall = nativeError.syscall;
+    if(nativeError) 
+    {
+      if ('signal'  in nativeError && nativeError?.signal)  this.signal  = nativeError.signal;
+      if ('address' in nativeError && nativeError?.address) this.address = nativeError.address;
+      if ('dest'    in nativeError && nativeError?.dest)    this.dest    = nativeError.dest;
+      if ('errno'   in nativeError && nativeError?.errno)   this.errno   = nativeError.errno;
+      if ('info'    in nativeError && nativeError?.info)    this.info    = nativeError.info;
+      if ('path'    in nativeError && nativeError?.path)    this.path    = nativeError.path;
+      if ('port'    in nativeError && nativeError?.port)    this.port    = nativeError.port;
+      if ('syscall' in nativeError && nativeError?.syscall) this.syscall = nativeError.syscall;
+    }
   }
 
+  /**
+   * @param {Record<string, unknown>} detail
+   */
   SetDetail (detail)
   {
     this.detail = detail;
     return this;
   };
 
+  /**
+   * @param {boolean} skipLog
+   */
   SetSkipLog (skipLog)
   {
     this.skipLog = skipLog;
@@ -67,47 +99,56 @@ class SquidError extends Error
   * Based on `dumpException` in
   * https://github.com/davepacheco/node-extsprintf/blob/master/lib/extsprintf.js
   */
-  static GetFullErrorStack (exception)
+  /**
+   * @param {Error} error
+   */
+  static GetFullErrorStack (error)
   {
-    let fullErrorStack = exception.stack || exception.toString();
+    let fullErrorStack = error.stack || error.toString();
 
-    if (exception.cause && typeof (exception.cause) === 'function')
+    if ('cause' in error && error.cause && typeof (error.cause) === 'function')
     {
-      const exceptionCause = exception.cause();
+      const exceptionCause = error.cause();
 
       if (exceptionCause)
         fullErrorStack += `\nCaused by: ${this.GetFullErrorStack(exceptionCause)}`;
     }
 
-    return (fullErrorStack);
+    return fullErrorStack;
   }
 
+  /**
+   * @param {Error} error
+   */
   static SerializeNativeError (error)
   {
     return {
       message : error.message,
       name    : error.name,
-      code    : error.code,
+      code    : 'code' in error && error.code,
       ...(error.stack && { stack : this.GetFullErrorStack(error) }),
       // these are SystemError properties. Since NodeJS doesnt expose
       // the SystemError type, we cannot simply check for
       // if (err instanceof SystemError) and then add all
       // these properties at once. Thus, we need to check the existence
       // of each one individually
-      ...(error.signal  && { signal  : error.signal  }),
-      ...(error.address && { address : error.address }),
-      ...(error.dest    && { dest    : error.dest    }),
-      ...(error.errno   && { errno   : error.errno   }),
-      ...(error.info    && { info    : error.info    }),
-      ...(error.path    && { path    : error.path    }),
-      ...(error.port    && { port    : error.port    }),
-      ...(error.syscall && { syscall : error.syscall })
+      ...('signal'  in error && error.signal  && { signal  : error.signal  }),
+      ...('address' in error && error.address && { address : error.address }),
+      ...('dest'    in error && error.dest    && { dest    : error.dest    }),
+      ...('errno'   in error && error.errno   && { errno   : error.errno   }),
+      ...('info'    in error && error.info    && { info    : error.info    }),
+      ...('path'    in error && error.path    && { path    : error.path    }),
+      ...('port'    in error && error.port    && { port    : error.port    }),
+      ...('syscall' in error && error.syscall && { syscall : error.syscall })
     };
   }
 
+  /**
+   * @param {Error} error
+   */
   static Serialize (error)
   {
-    if(typeof error?.Serialize === 'function')
+    if('Serialize' in error && typeof error?.Serialize === 'function')
       return error.Serialize();
 
     if (Object.prototype.toString.call(error) === "[object Error]")
@@ -126,21 +167,38 @@ class SquidError extends Error
     };
   }
 
+  /**
+   * 
+   * @param {SquidErrorSettings} settings 
+   * @param {unknown} [nativeError] Erro original
+   * @returns 
+   */
   static Create (settings, nativeError)
   {
-    return new this(settings, nativeError, this.Create);
+    const validatedError = nativeError instanceof Error ? nativeError : undefined;
+
+    return new this(settings, validatedError, this.Create);
   }
 
+  /**
+   * @param {{ isSquidError?: boolean; }} error
+   */
   static IsSquidError(error)
   {
     return (error?.isSquidError === true);
   }
 
+  /**
+   * @param {unknown} error
+   */
   static ExactInstanceOf (error)
   {
     return (error instanceof this.constructor || error.constructor.name === this.name);
   }
 
+  /**
+   * @param {unknown} error
+   */
   static Convert (error, onlyConvertNonSquidErrors = false)
   {
     if (onlyConvertNonSquidErrors)
@@ -152,10 +210,16 @@ class SquidError extends Error
 
 class SquidHttpError extends SquidError
 {
+  /**
+   * @param {SquidHttpErrorSettings} settings
+   * @param {Error} nativeError
+   * @param {any} implementationContext
+   */
   constructor (settings, nativeError, implementationContext)
   {
     super(settings, nativeError, implementationContext);
 
+    /** @type {number} */
     this.httpStatusCode = (settings?.httpStatusCode || 500);
   }
 
@@ -167,6 +231,9 @@ class SquidHttpError extends SquidError
     };
   }
 
+  /**
+   * @param {number} status
+   */
   SetHttpStatusCode (status)
   {
     this.httpStatusCode = status;
